cd980feba32e722c9499b26bdf82f77a985c9c15
Brian Simiyu <briansimiyuj@gmail.com>
2024-11-28 12:39:51 +0300

    Left Hero 2

--- src/components/Hero/LeftHero.tsx
+++ src/components/Hero/LeftHero.tsx
@@ -1,3 +1,5 @@
+import { assets } from "../../assets/frontend/assets"
+
 const LeftHero: React.FC = ()=>{
 
     return(
@@ -6,6 +8,15 @@ const LeftHero: React.FC = ()=>{
 
             <h3 className="text-3xl md:text-4xl lg:text-5xl text-white font-bold leading-tight md:leading-tight lg:leading-tight"
             >Book Appointment <br/> With Trusted Doctors</h3>
+
+
+            <div className="flex flex-col md:flex-row items-center gap-3 text-secondary-bg text-sm font-medium">
+
+                <img src={assets.groupProfilePic} alt="group-profile-pic" className="w-28" />
+
+                <p>MediCare Hub is a user-friendly platform that streamlines online appointment booking, connecting patients with doctors and specialists effortlessly. <br/> With a few simple steps, you can browse available slots, choose a provider, and confirm your appointment, making healthcare more accessible and convenient than ever.</p>
+
+            </div>
 
         </div>
 
