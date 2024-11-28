39296a4bbd81f0076209b39b25bc1de1716fc5c1
Brian Simiyu <briansimiyuj@gmail.com>
2024-11-28 12:12:46 +0300

    Left Hero 1

--- README.md
+++ README.md
@@ -45,3 +45,4 @@ Configure Tailwind CSS and and theme colors in `tailwind.config.js` file.
   The Home page is the main page of the application. It includes a hero section, a speciality menu, a top doctors section, and a footer.
 
     1. Create a Hero component and mount it on Home page
+        a. Create left side of the hero section which will have a heading, a paragraph, and a button
---
+++ src/components/Hero/LeftHero.tsx
@@ -0,0 +1,16 @@
+const LeftHero: React.FC = ()=>{
+
+    return(
+
+        <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 m-auto md:py-[10vw] md:mb-[-30px]">
+
+            <h3 className="text-3xl md:text-4xl lg:text-5xl text-white font-bold leading-tight md:leading-tight lg:leading-tight"
+            >Book Appointment <br/> With Trusted Doctors</h3>
+
+        </div>
+
+    )
+
+}
+
+export default LeftHero
