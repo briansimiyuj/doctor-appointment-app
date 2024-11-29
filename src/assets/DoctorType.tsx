cae504dc097a08af8de7c151778e95eeee28cfd2
Brian Simiyu <briansimiyuj@gmail.com>
2024-11-29 14:35:58 +0300

    Doctor Type

---
+++ src/assets/DoctorType.tsx
@@ -0,0 +1,19 @@
+export type DoctorType ={
+
+    _id: string
+    name: string
+    image: string
+    speciality: string
+    degree: string
+    experience: string
+    about: string
+    fees: number
+
+    address:{
+
+        line1: string
+        line2: string
+
+    }
+
+}
