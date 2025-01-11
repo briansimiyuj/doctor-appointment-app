b28fb367d6bc2bf776d71bd98d7ccc53b0a597b2
Brian Simiyu <briansimiyuj@gmail.com>
2025-01-11 14:39:42 +0300

    Enhance booking components by adding time slot selection and improving layout

--- src/components/Bookings/BookingSlots.tsx
+++ src/components/Bookings/BookingSlots.tsx
@@ -9,17 +9,23 @@ const BookingSlots: React.FC = ()=>{
 
             <h2>Booking Slots</h2>
 
-            <div className="flex lg:flex-col gap-4 h-[900px] lg:h-[300px] overflow-hidden">
+            <form>
 
-                <BookingDays/>
+                <div className="flex lg:flex-col gap-4 h-[900px] lg:h-[300px] overflow-hidden">
 
-                <BookingTime/>
+                    <BookingDays/>
+
+                    <BookingTime/>
+
+                </div>
+
+                <button 
+                    className="bg-primary-bg text-white px-8 py-2.5 rounded-full mt-6 hover:bg-blue-600 transition-all duration-300 ease-in-out              active:scale-95 shadow-md hover:shadow-lg text-sm sm:text-base font-medium disabled:bg-gray-400 disabled:cursor-not-allowed"
+                >Book an appointment</button>
+
+            </form>
 
 
-            </div>
-
-
-            <button className="bg-primary-bg text-white px-8 py-2.5 rounded-full mt-6 hover:bg-blue-600 transition-all duration-300 ease-in-out              active:scale-95 shadow-md hover:shadow-lg text-sm sm:text-base font-medium disabled:bg-gray-400 disabled:cursor-not-allowed">Book an appointment</button>
 
         </div>
 
