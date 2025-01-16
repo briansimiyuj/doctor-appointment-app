9e635aba59ac83465c6a7d7d62818565812a1040
Brian Simiyu <briansimiyuj@gmail.com>
2025-01-16 14:12:02 +0300

    Update MyAppointmentsPage to retrieve and display appointed doctors from local storage

--- src/pages/MyAppointmentsPage.tsx
+++ src/pages/MyAppointmentsPage.tsx
@@ -1,7 +1,20 @@
-import { doctors } from "../assets/frontend/assets"
 import AppointmentCard from "../components/Appointments/AppointmentCard"
+import { AppointedDoctorType } from "../assets/types/AppointedDoctorType"
+import { useBookingSlots } from "../hooks/useBookingSlots"
+import { BookingContext } from "../context/BookingContext"
+import { useContext, useEffect } from "react"
 
 const MyAppointmentsPage: React.FC = ()=>{
+
+    const { appointedDoctors } = useContext(BookingContext)
+
+    useEffect(() =>{
+    
+       console.log('Updated Doctors', appointedDoctors)
+    
+    }, [appointedDoctors])
+
+    return
 
     return(
 
@@ -9,14 +22,20 @@ const MyAppointmentsPage: React.FC = ()=>{
         
             <h2 className="pb-3 mt-12 font-medium text-zinc-700 boorder-b">My Appointments</h2>
 
-
             {
+                doctors && doctors.length > 0 ?(
 
-                doctors.slice(0, 3).map((doctor, index)=>(
+                    doctors.map((doctor: AppointedDoctorType, index: number) =>(
 
-                    <AppointmentCard doctor={doctor} key={index}/>
+                        <AppointmentCard doctor={doctor} key={index}/>
+                        
+                    ))
 
-                ))
+                ):(
+
+                    <p className="text-center text-gray-500">You have no appointments</p>
+
+                )
 
             }
         
