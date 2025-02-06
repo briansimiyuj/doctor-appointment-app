6f74c357b12f3f0717f877acf58d2cd439ebd6e1
Brian Simiyu <briansimiyuj@gmail.com>
2025-02-06 11:28:43 +0300

    Add patient data management with JSON and update context integration

---
+++ src/assets/types/PatientType.ts
@@ -0,0 +1,26 @@
+export type PatientType ={
+
+    _id: string
+    name: string
+    age: number
+    gender: string
+    status: string
+    image: string
+    
+    contact:{
+        phone: string
+        email: string
+    }
+    
+    address:{
+        line1: string
+        line2: string
+    }
+
+    appointment:{
+        date: string
+        time: string
+        doctor: string
+        speciality: string
+    }
+}
