daedae5a6302d0c8144cfbc613bf25bba70f0141
Brian Simiyu <briansimiyuj@gmail.com>
2025-02-08 16:25:54 +0300

    Add Weekly Calendar component and integrate with ScheduleContext

---
+++ src/components/Schedule/WeeklyCalendar.tsx
@@ -0,0 +1,59 @@
+import { useSchedule } from "../../context/ScheduleContext"
+
+const WeeklyCalendar: React.FC = ()=>{
+
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+          { schedule } = useSchedule()
+
+    console.log(schedule.availableSlots[0].slots)
+
+    return(
+
+        <div className="w-full rounded-lg shadow p-4 bg-primary-bg sm:p-6">
+
+            <h2 className="text-xl font-semibold mb-4">Weekly Schedule</h2>
+
+            <div className="flex flex-col sm:grid sm:grid-cols-7 gap-4 ">
+ 
+                {
+
+                    days.map((day, index) =>(
+
+                        <div key={index} className="flex md:flex-col items-center border-b sm:border-0 pb-2 gap-6 px-4 sm:pb-0">
+
+                            <h3 className="font-medium text-base md:text-lg sm:mb-2">{day}</h3>
+
+                            <div className="flex sm:block gap-2 sm:space-y-2 flex-wrap">
+
+                                {
+
+                                    schedule.availableSlots[index].slots.map((daySlots, index) =>(
+
+                                        <div key={index} className="bg-white rounded-lg px-2 py-1">
+
+                                            {daySlots}
+
+                                        </div>
+
+                                    ))
+
+                                }
+
+                            </div>
+
+                        </div>
+
+                    ))
+
+                }
+
+            </div>
+
+        </div>
+
+
+    )
+
+}
+
+export default WeeklyCalendar
