1e8e7182dd3bb6b1d92dac8539b7758a8735501a
Brian Simiyu <briansimiyuj@gmail.com>
2025-02-12 15:13:26 +0300

    Refactor ScheduleContextProps to include preferences and break time structure

--- src/assets/contextProps/ScheduleContextProps.ts
+++ src/assets/contextProps/ScheduleContextProps.ts
@@ -1,41 +1,60 @@
 import { TimeSlotType } from "../types/TimeSlotType"
 
-export interface ScheduleContextProps{
+export interface ScheduleContextProps {
 
-    schedule:{
+    schedule: {
 
-        workingHours:{
-
+        workingHours: { 
             start: string
-            end: string
-
+            end: string 
         }
 
-        availableSlots:{
+        preferences: {
+            slotDuration: number
+            maxPatientsPerDay: number
+            autoConfirmation: boolean
+        }
 
+        availableSlots: { 
             date: string
-            slots: string[]
-
+            slots: string[] 
         }[]
 
         blockedDates: string[]
 
-        breakTime:{
-
+        breakTime: { 
             start: string
-            end: string
-
+            end: string 
         }
 
     }
 
-    preferences:{
+    setSchedule: (schedule: {
 
-        slotDuration: number
-        maxPatientsPerDay: number
-        autoConfirmation: boolean
-        
-    }
+        workingHours: { 
+            start: string
+            end: string 
+        }
+
+        preferences: {
+            slotDuration: number
+            maxPatientsPerDay: number
+            autoConfirmation: boolean
+        }
+
+        availableSlots: { 
+            date: string
+            slots: string[] 
+        }[]
+
+        blockedDates: string[]
+
+        breakTime: { 
+            start: string
+            end: string 
+        }
+
+    }) => void
 
     slotIndex: number
     setSlotIndex: (index: number) => void
@@ -43,7 +62,5 @@ export interface ScheduleContextProps{
     setSlotTime: (time: string) => void
     selectedTimeSlot: TimeSlotType | null
     setSelectedTimeSlot: (slot: TimeSlotType | null) => void
-    setSchedule: (schedule: ScheduleContextProps['schedule']) => void
-    setPreferences: (preferences: ScheduleContextProps['preferences']) => void
 
 }
