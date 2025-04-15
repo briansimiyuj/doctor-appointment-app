b3ed308e1ba290d7c6658789c59da397b4e73d67
Brian Simiyu <briansimiyuj@gmail.com>
2025-04-15 15:10:02 +0300

    Mount cancel appointment modal component on TabActionButton

---
+++ src/assets/DummyAppointment.ts
@@ -0,0 +1,22 @@
+import { AppointmentType } from "./types/AppointmentType";
+
+export const DummyAppointment: AppointmentType = {
+    
+    date: "2023-06-15",
+    time: "10:00 AM",
+    status: "confirmed",
+    
+    patient:{
+        _id: "dev-patient-id",
+        name: "Development Patient",
+        image: "",
+    },
+
+    doctor:{
+        _id: "dev-doctor-id",
+        name: "Dr. Developer",
+        image: ""
+    },
+    consultationType: "in-person",
+    
+}
