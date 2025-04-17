2d24d80e609224a9f2b2582153840d8615c35e4c
Brian Simiyu <briansimiyuj@gmail.com>
2025-04-17 12:48:14 +0300

    Implement ReasonInput component in Cancel Appointment Modal

--- src/components/PatientDetails/CancelModals/ModalBody.tsx
+++ src/components/PatientDetails/CancelModals/ModalBody.tsx
@@ -1,4 +1,5 @@
 import { useModalContext } from "../../../context/ModalContext"
+import ReasonInput from "./ReasonInput"
 
 const ModalBody: React.FC = ()=>{
 
@@ -15,17 +16,7 @@ const ModalBody: React.FC = ()=>{
             </p>
 
 
-            <div className="mt-4">
-            
-                <label className="block text-gray-700 font-medium mb-2">Reason for Cancellation</label>
-
-                <textarea
-                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-bg"
-                    placeholder="Enter reason for cancellation"
-                    rows={4}
-                />
-            
-            </div> 
+            <ReasonInput/>
 
         </div>
 
