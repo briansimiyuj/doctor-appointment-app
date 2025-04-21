e8450bccb2955a95b13273b93c3160ac50ee3b90
Brian Simiyu <briansimiyuj@gmail.com>
2025-04-21 12:31:01 +0300

    Implement Modal Body to the Reject Appointment Modal

--- src/components/PatientDetails/RejectModal/RejectAppointmentModal.tsx
+++ src/components/PatientDetails/RejectModal/RejectAppointmentModal.tsx
@@ -1,8 +1,26 @@
+import { ModalProvider } from "../../../context/ModalContext"
+import ModalBody from "./ModalBody"
+
 const RejectAppointmentModal: React.FC<{ appointment: any }> = ({ appointment })=>{ 
 
     return(
 
-        <h1>RejectAppointmentModal</h1>
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+
+
+            <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md mx-4">
+
+                <h2 className="text-xl font-bold mb-4 text-center text-gray-800">Reject Appointment</h2>
+
+                <ModalProvider appointment={appointment}>
+
+                    <ModalBody/>
+
+                </ModalProvider>
+
+            </div>
+
+        </div>
 
     )
 
