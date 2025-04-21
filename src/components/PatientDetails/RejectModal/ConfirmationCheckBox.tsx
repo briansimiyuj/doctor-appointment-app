d3589a0c5583ced1099f533acb41612c6b4e68d2
Brian Simiyu <briansimiyuj@gmail.com>
2025-04-21 12:55:30 +0300

    Implement Confirmation Checkbox Component in Reject Modal

---
+++ src/components/PatientDetails/RejectModal/ConfirmationCheckBox.tsx
@@ -0,0 +1,41 @@
+import { useModalContext } from "../../../context/ModalContext"
+
+const ConfirmationCheckBox: React.FC = ()=>{
+
+    const { isConfirmed, setIsConfirmed } = useModalContext()
+
+    return(
+
+        <div className="mb-6">
+
+            <label htmlFor="checkbox" className="flex items-center">
+
+                <input
+                    className="form-checkbox h-5 w-5 text-primary-bg rounded"
+                    type="checkbox"
+                    checked={isConfirmed}
+                    onChange={e => setIsConfirmed(e.target.checked)}
+                />
+
+                <span className="ml-2 text-gray-700">I understand that rejecting this appointment may affect the patient's care plan.</span>
+
+            </label>
+
+
+            {
+
+                !isConfirmed &&(
+
+                    <p className="text-red-500 text-sm mt-1">Please confirm rejection</p>
+
+                )
+
+            }
+
+        </div>
+
+    )
+
+}
+
+export default ConfirmationCheckBox
