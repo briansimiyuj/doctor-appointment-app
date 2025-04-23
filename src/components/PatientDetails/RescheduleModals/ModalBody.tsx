5534d654674180287c58c4a3f7a5f463871be425
Brian Simiyu <briansimiyuj@gmail.com>
2025-04-23 15:47:59 +0300

    Feat: Implement modal body component for reschedule modal

---
+++ src/components/PatientDetails/RescheduleModals/ModalBody.tsx
@@ -0,0 +1,27 @@
+import { useRescheduleModal } from "../../../context/RescheduleModalContext"
+
+const ModalBody: React.FC = ()=>{
+
+    const { appointment } = useRescheduleModal()
+
+    return(
+
+        <>
+
+            <div className="mb-4">
+
+                <p className="text-gray-700">
+
+                    You are rescheduling the appointment originally scheduled for <span className="font-medium">{appointment?.date}</span> at <span className="font-medium">{appointment?.time}</span>.
+
+                </p>
+
+            </div>
+
+        </>
+
+    )
+
+}
+
+export default ModalBody
