6023cc0099959763b936d8d140461beeb1a7fdf5
Brian Simiyu <briansimiyuj@gmail.com>
2025-05-01 11:44:09 +0300

    Feat: Integrate time selection and display in TimePicker

--- src/components/PatientDetails/RescheduleModals/TimePicker/TimePicker.tsx
+++ src/components/PatientDetails/RescheduleModals/TimePicker/TimePicker.tsx
@@ -1,3 +1,5 @@
+import SelectedTimeDisplay from "./SelectedTimeDisplay"
+
 const TimePicker: React.FC = ()=>{
 
     return(
@@ -5,6 +7,12 @@ const TimePicker: React.FC = ()=>{
         <div className="mb-4">
 
             <label className="block text-gray-700 text-sm font-medium mb-2">Select New Time:</label>
+
+            <div className="bg-white p-4 rounded-lg shadow-md">
+
+                <SelectedTimeDisplay/>
+
+            </div>
 
         </div>
 
