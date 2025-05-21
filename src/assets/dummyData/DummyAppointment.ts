efb2489a75189c7fff461a49183fed693f717fe1
Brian Simiyu <briansimiyuj@gmail.com>
2025-05-21 10:39:01 +0300

    Fix: Update dummy appointment data

--- src/assets/dummyData/DummyAppointment.ts
+++ src/assets/dummyData/DummyAppointment.ts
@@ -1,21 +1,23 @@
 import { AppointmentType } from "../types/AppointmentType"
+import { doctorImages, patientImages } from "../frontend/assets"
 
 export const DummyAppointment: AppointmentType ={
-    
+
+    _id: "fc",
     date: "2023-06-15",
     time: "10:00 AM",
-    status: "confirmed",
+    status: "cancelled",
     
     patient:{
         _id: "dev-patient-id",
         name: "Development Patient",
-        image: "",
+        image: patientImages.patient5,
     },
 
     doctor:{
         _id: "dev-doctor-id",
         name: "Dr. Developer",
-        image: ""
+        image: doctorImages.doc13,
     },
     consultationType: "in-person",
     
