e3daa1beb9293276b5a0ffa3e99927c7ab1edc30
Brian Simiyu <briansimiyuj@gmail.com>
2025-06-05 13:27:39 +0300

    Feat: Mount ModalHeader component on the Add Notes Modal component

--- src/components/PatientDetails/AddNotesModals/AddNotesModal.tsx
+++ src/components/PatientDetails/AddNotesModals/AddNotesModal.tsx
@@ -1,3 +1,5 @@
+import ModalHeader from "../ModalHeader"
+
 interface AddNotesModalProps{
     
     onClose: () => void
@@ -8,7 +10,15 @@ const AddNotesModal: React.FC<AddNotesModalProps> = ({ onClose }) =>{
 
     return(
 
-        <h1>AddNotesModal</h1>
+        <div className="fixed inset-0 flex items-center justify-center z-50 p-4 bg-black bg-opacity-50">
+
+            <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-2xl mx-auto overflow-y-auto max-h-[90vh] flex flex-col">
+
+                <ModalHeader title="Add Notes" onClose={onClose}/>
+
+            </div>
+
+        </div>
 
     )
 
