1a6d3d9b3952f4263e3696222110f85eb55e433e
Brian Simiyu <briansimiyuj@gmail.com>
2025-06-18 14:07:33 +0300

    feat: Enhance Selected File Card to display file name, type, and size

--- src/components/PatientDetails/Tabs/DocumentTab/SelectedFileCard.tsx
+++ src/components/PatientDetails/Tabs/DocumentTab/SelectedFileCard.tsx
@@ -17,17 +17,21 @@ const SelectedFileCard: React.FC<SelectedFileCardProps> = ({ file, })=>{
             <div className="flex items-center gap-3">
 
                 <FiFile className="w-5 h-5 text-gray-500"/>
+          
+                <div className="min-w-0">
 
-                <div>
+                    <div>
+                    
+                        <p className="text-xs sm:text-sm font-medium text-gray-800 break-words max-w-[100px] sm:max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg 2xl:max-w-xl">{file.name}</p>
 
-                    <p className="text-sm font-medium text-gray-800">{file.name}</p>
+                        <p className="text-xs text-gray-500">
 
-                    <p className="text-xs text-gray-500">
+                            {file.type} • {(file.size / 1024 / 1024).toFixed(2)} MB
 
-                        {file.type} • {(file.size / 1024 / 1024).toFixed(2)} MB
+                        </p>
 
-                    </p>
-
+                    </div>
+                    
                 </div>
 
             </div>
