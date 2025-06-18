8b0dff136f2df918caff57ecb65bbb115c5d7df5
Brian Simiyu <briansimiyuj@gmail.com>
2025-06-18 13:53:12 +0300

    feat: Add Selected Files component to display selected documents with file details

--- src/components/PatientDetails/Tabs/DocumentTab/SelectedFiles.tsx
+++ src/components/PatientDetails/Tabs/DocumentTab/SelectedFiles.tsx
@@ -1,10 +1,33 @@
+import { useDocumentsTab } from "../../../../context/DocumentsTabContext"
+import SelectedFileCard from "./SelectedFileCard"
+
 const SelectedFiles: React.FC = ()=>{
 
+    const { selectedFiles } = useDocumentsTab()
+    
     return(
 
         <div className="mt-4 text-left">
 
             <h4 className="font-medium mb-2 text-gray-700">Selected Files:</h4>
+
+            <div className="space-y-2">
+
+                {
+
+                    selectedFiles.map((file, index)=>(
+
+                        <SelectedFileCard
+                            key={index}
+                            file={file}                        
+                            index={index}
+                        />
+
+                    ))
+
+                }
+
+            </div>
 
         </div>
 
