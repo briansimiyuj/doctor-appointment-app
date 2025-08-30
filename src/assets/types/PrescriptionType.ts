60ea374af813af901482a14f50a498f8247076e2
Brian Simiyu <briansimiyuj@gmail.com>
2025-08-30 16:25:35 +0300

    feat(prescriptions): Add prescription management functionality to PatientDetails context bug

--- src/assets/types/PrescriptionType.ts
+++ src/assets/types/PrescriptionType.ts
@@ -1,13 +1,9 @@
 export interface PrescriptionType{
 
-    _id: string
     medicineName: string
     dose: string
     frequency: string
     duration: string
     notes?: string
-    appointmentID: string
-    doctorName: string
-    date: string
     
 }
